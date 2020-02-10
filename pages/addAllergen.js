c0a34975fbedda5c7bfb9899b009cb1367bf61c2
AoifeMcNeill <amcneill849@gmail.com>
2020-02-10 20:51:00 +0000

    fixed typos

--- pages/addAllergen.js
+++ pages/addAllergen.js
@@ -5,7 +5,7 @@ import {
   Linking,
   TouchableHighlight,
   Platform,
-  Stylesheet,
+  StyleSheet,
   Scrollview,
 } from 'react-native';
 import {Header, Icon} from 'react-native-elements';
@@ -14,39 +14,42 @@ export default class addAllergen extends Component {
   constructor() {
     super();
   }
-}
 
-render() {
-    return(
-        <Scrollview>
-            <View>
-                <Header
-                leftComponent={{icon: 'menu', color: '#fff'}}
-                centerComponent={{text: 'Allergen Filter', style: {color: '#fff'}}}
-                rightComponent={{icon: 'home', color: '#fff'}}
-                />
-            </View>
-            <Text style={styles.headingText}>Simply type in the ingredient you want filtered and press the add button!</Text>
-        </Scrollview>
+  render() {
+    return (
+      <Scrollview>
+        <View>
+          <Header
+            leftComponent={{icon: 'menu', color: '#fff'}}
+            centerComponent={{text: 'Allergen Filter', style: {color: '#fff'}}}
+            rightComponent={{icon: 'home', color: '#fff'}}
+          />
+        </View>
+        <Text style={styles.headingText}>
+          Simply type in the ingredient you want filtered and press the add
+          button!
+        </Text>
+      </Scrollview>
     );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 10,
-      },
-      headingText: {
-        color: 'black',
-        fontSize: 24,
-        alignSelf: 'center',
-        marginLeft: 5,
-        marginRight: 5,
-        marginTop: 30,
-        fontWeight: 'bold',
-        flexWrap: 'wrap',
-      },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+  headingText: {
+    color: 'black',
+    fontSize: 24,
+    alignSelf: 'center',
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 30,
+    fontWeight: 'bold',
+    flexWrap: 'wrap',
+  },
 });
