d64a2acd1daa8947892ab5ea360d018b0ff27db1
AoifeMcNeill <amcneill849@gmail.com>
2020-02-10 20:24:23 +0000

    add pages folder, addAllergen.js page and basic set up for the page

---
+++ pages/addAllergen.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {
+  Text,
+  View,
+  Linking,
+  TouchableHighlight,
+  Platform,
+  Stylesheet,
+  Scrollview,
+} from 'react-native';
+import {Header, Icon} from 'react-native-elements';
+
+export default class addAllergen extends Component {
+  constructor() {
+    super();
+  }
+}
