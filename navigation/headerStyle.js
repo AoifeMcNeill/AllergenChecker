2386f8aa60473a7b4261ec1c4d895e76221fc7f1
AoifeMcNeill <amcneill849@gmail.com>
2020-02-12 09:32:41 +0000

    Filled in headerStyle.js; added comments

---
+++ navigation/headerStyle.js
@@ -0,0 +1,12 @@
+//Style for the header section that's seen across all pages
+//The hex colour used is a type of blue called "aqua pearl"
+export const headerStyle = {
+  fontFamily: 'sans-serif-medium',
+  fontWeight: 'normal',
+  fontSize: 24,
+  textAlign: 'center',
+  flex: 1,
+  marginTop: 5,
+};
+
+export const mainColor = '#6daad9';
