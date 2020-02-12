c68c74e5a7bba1b1d46bdf43eb2994e469cfa71d
AoifeMcNeill <amcneill849@gmail.com>
2020-02-12 09:21:01 +0000

    added more folder directories, and menuButton

---
+++ navigation/menuButton.js
@@ -0,0 +1,30 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
+import {View, TouchableOpacity, Image} from 'react-native';
+
+class menuButton extends Component {
+  toggleDrawer = () => {
+    this.props.navigationProps.toggleDrawer();
+  };
+
+  render() {
+    return (
+      <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity onPress={this.toggleDrawer.bind(this)}>
+          <Image
+            source={{
+              uri:
+                'https://reactnativecode.com/wp-content/uploads/2018/04/hamburger_icon.png',
+            }}
+            style={{
+              width: 25,
+              height: 25,
+              marginLeft: 25,
+              tintColor: '#A5ACAA',
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
