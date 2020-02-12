0f061a3dc50d7b154e401a8f3248354ebfdd5fe8
AoifeMcNeill <amcneill849@gmail.com>
2020-02-12 09:42:46 +0000

    Filled homeNav.js; added comments

---
+++ navigation/homeNav.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {createStackNavigator} from 'react-navigation';
+import {View} from 'react-native';
+import menuButton from './menuButton';
+import BarcodeScanner from '../pages/scanner';
+import {headerStyle, mainColor} from './HeaderStyle';
+
+//Navigation menu stack opens upon pressing "Hamburger" icon.
+const homeNav = createStackNavigator({
+  Home: {
+    screen: BarcodeScanner,
+    navigationOptions: ({navigation}) => ({
+      title: 'Scanner',
+      headerLeft: <menuButton navigationProps={navigation} />,
+      headerRight: <View />,
+      headerTitleStyle: headerStyle,
+      headerTintColor: mainColor,
+      headerStyle: {
+        backgroundColor: '#fff',
+      },
+    }),
+  },
+});
+
+export default homeNav;
