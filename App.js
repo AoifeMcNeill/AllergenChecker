80826192f18e1ab0544cac1e09d439ad16092a95
AoifeMcNeill <amcneill849@gmail.com>
2020-02-15 12:04:54 +0000

    fixed syntax for app.js

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import nav from './navigation/nav';
 
-
 export default class App extends Component {
-
-    render() {
-        return (
-            <nav />
-        );
-    }
+  render() {
+    return <nav />;
+  }
 }
