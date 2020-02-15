1cad45bbfa1ad7dd72cf46b0efba6ee2c35fa860
AoifeMcNeill <amcneill849@gmail.com>
2020-02-15 10:10:40 +0000

    fixed indentation and syntax; added comments

--- navigation/addAllergenNav.js
+++ navigation/addAllergenNav.js
@@ -1,24 +1,26 @@
+//Nav menu for addAllergen
+
 import React from 'react';
-import { createStackNavigator } from 'react-navigation';
-import { View } from 'react-native';
-import SearchScreen from '../pages/addAllergen';
+import {createStackNavigator} from 'react-navigation';
+import {View} from 'react-native';
+import addAllergen from '../pages/addAllergen';
 import menuButton from './menuButton';
-import { headerStyle, mainColor } from './headerStyle';
+import {headerStyle, mainColor} from './headerStyle';
 
 const addAllergenNav = createStackNavigator({
-    addAllergen: {
-        screen: addAllergenScreen,
-        navigationOptions: ({ navigation }) => ({
-            title: 'Add Allergen/Intolerance',
-            headerLeft : <menuButton navigationProps={ navigation }/>,
-            headerRight: <View></View>,
-            headerTitleStyle: headerStyle,
-            headerTintColor: mainColor,
-            headerStyle: {
-                backgroundColor: '#fff',
-            }
-        })
-    },
+  addAllergen: {
+    screen: addAllergen,
+    navigationOptions: ({navigation}) => ({
+      title: 'Add Allergen/Intolerance',
+      headerLeft: <menuButton navigationProps={navigation} />,
+      headerRight: <View />,
+      headerTitleStyle: headerStyle,
+      headerTintColor: mainColor,
+      headerStyle: {
+        backgroundColor: '#fff',
+      },
+    }),
+  },
 });
 
 export default addAllergenNav;
