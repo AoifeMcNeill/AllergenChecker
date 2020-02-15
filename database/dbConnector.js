6f349cee651df58e6e61ea41201b2a141f4f1da3
AoifeMcNeill <amcneill849@gmail.com>
2020-02-15 11:52:43 +0000

    couldn't get realm to install on latest npm version, and not willing to miss with versions in case it causes issues for other react native libraries, so commented out for later change

--- database/dbConnector.js
+++ database/dbConnector.js
@@ -1,27 +1,27 @@
-import Realm from 'realm';
+//import Realm from 'realm';
 
 //Database for Allergens and User
 
-class Allergen extends Realm.Object {}
+//class Allergen extends Realm.Object {}
 
-Allergen.schema = {
-  name: 'Allergen',
-  properties: {
-    id: 'string',
-    name: 'string',
-  },
-};
+//Allergen.schema = {
+//name: 'Allergen',
+//properties: {
+//id: 'string',
+//name: 'string',
+//},
+//};
 
-class User extends Realm.Object {}
+//class User extends Realm.Object {}
 
-User.schema = {
-  name: 'User',
-  primaryKey: 'username',
-  properties: {
-    username: 'string',
-    allergies: 'Allergen[]',
-    updatedAt: 'date?',
-  },
-};
+//User.schema = {
+//name: 'User',
+//primaryKey: 'username',
+//properties: {
+//username: 'string',
+//allergies: 'Allergen[]',
+//updatedAt: 'date?',
+//},
+//};
 
-export default new Realm({schema: [Allergen, User], schemaVersion: 20});
+//export default new Realm({schema: [Allergen, User], schemaVersion: 20});
