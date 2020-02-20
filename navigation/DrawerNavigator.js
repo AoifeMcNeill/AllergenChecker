dd26c0be6892f7edc69151d6af5e83cd8e85bff8
AoifeMcNeill <amcneill849@gmail.com>
2020-02-20 15:46:23 +0000

    test 7

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -1,16 +1,16 @@
-//import React from 'react';
+import React from 'react';
 
-//import {View, Text} from 'react-native';
-//import {createDrawerNavigator} from 'react-navigation-drawer';
+import {View, Text} from 'react-native';
+import {createDrawerNavigator} from 'react-navigation-drawer';
 
-//import scanner from 'AllergenChecker/pages/scanner';
-//import addAllergen from 'AllergenChecker/pages/addAllergen';
-//import App from 'AllergenChecker/App';
+import scanner from 'AllergenChecker/pages/scanner';
+import addAllergen from 'AllergenChecker/pages/addAllergen';
+import App from 'AllergenChecker/App';
 
-//const DrawerNavigator = createDrawerNavigator({
-  //scanner: scanner,
-  //addAllergen: addAllergen,
-  //App: App,
-//});
+const DrawerNavigator = createDrawerNavigator({
+  scanner: scanner,
+  addAllergen: addAllergen,
+  App: App,
+});
 
-//export default DrawerNavigator;
+export default DrawerNavigator;
