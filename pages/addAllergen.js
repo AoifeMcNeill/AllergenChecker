4db4c1d2cccff7ac78ab270adfc69c8c3f130559
AoifeMcNeill <amcneill849@gmail.com>
2020-03-05 10:24:22 +0000

    rebulding addAllergen.js

--- pages/addAllergen.js
+++ pages/addAllergen.js
@@ -5,124 +5,27 @@ import {
   FlatList,
   Text,
   View,
-  Alert,
   TouchableOpacity,
   TextInput,
 } from 'react-native';
 
 export default class addAllergen extends Component {
-  constructor(props) {
-    super(props);
-    (this.array = [])(
-      (this.state = {
-        arrayHolder: [],
-        textInput_Holder: '',
-      }),
-    );
-  }
-
-  ComponentDidMount() {
-    this.setState({arrayHolder: [this.array]});
-  }
-
-  joinData = () => {
-    this.array.push({title: this.state.textInput_Holder});
-    this.setState({arrayHolder: [...this.array]});
+  constructor() {
+    super();
+    this.state = {
+        addInput: [],
+        textInput: ''
+    }
   };
 
-  FlatListItemsSeparator = () => {
-    return (
-      <View
-        // eslint-disable-next-line react-native/no-inline-styles
-        style={{
-          height: 1,
-          width: '100%',
-          backgroundColor: '#607D8B',
-        }}
-      />
-    );
-  };
-
-  getItem(item) {
-    Alert.alert(item);
-  }
-
-  render() {
-    return (
-      <View style={styles.MainContainer}>
-        <TextInput
-          placeholder="Enter value here"
-          onChangeText={data => this.setState({textInput_Holder: data})}
-          style={styles.textInputStyle}
-          underlineColorAndroid="transparent"
-        />
-        <TouchableOpacity
-          onPress={this.joinData}
-          activeOpacity={0.7}
-          style={styles.button}>
-          <Text style={styles.buttonText}>Add values to FlatList</Text>
-        </TouchableOpacity>
-        <FlatList
-          data={this.state.arrayHolder}
-          width="100%"
-          extraData={this.state.arrayHolder}
-          keyExtractor={index => index.toString()}
-          ItemSeparatorComponent={this.FlatListItemsSeparator}
-          renderItem={({item}) => (
-            <Text
-              style={styles.item}
-              onPress={this.GetItem.bind(this, item.title)}>
-              {' '}
-              {item.title}{' '}
-            </Text>
-          )}
-        />
+  render(){
+    return(
+      <View>
+        <Text>Add a food that you are allergic/intolerant to:</Text>
+        <TextInput></TextInput>
+        <TouchableOpacity>Add</TouchableOpacity>
+        <FlatList></FlatList>
       </View>
-    );
+    )
   }
-}
-
-const styles = StyleSheet.create({
-  MainContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
-    margin: 2,
-  },
-
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-
-  textInputStyle: {
-    textAlign: 'center',
-    height: 40,
-    width: '90%',
-    borderWidth: 1,
-    borderColor: '#4CAF50',
-    borderRadius: 7,
-    marginTop: 12,
-  },
-
-  button: {
-    width: '90%',
-    height: 40,
-    padding: 10,
-    backgroundColor: '#4CAF50',
-    borderRadius: 8,
-    marginTop: 10,
-  },
-
-  buttonText: {
-    color: '#fff',
-    textAlign: 'center',
-  },
-});
+};
