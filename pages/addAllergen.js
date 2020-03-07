7472009c34ae40104a4b678581a128ac9020f3a6
AoifeMcNeill <amcneill849@gmail.com>
2020-03-07 13:49:08 +0000

    let's try this

--- pages/addAllergen.js
+++ pages/addAllergen.js
@@ -12,20 +12,64 @@ import {
 export default class addAllergen extends Component {
   constructor() {
     super();
-    this.state = {
-        addInput: [],
-        textInput: ''
+    state = {
+        textInputs: [],
     }
   };
 
   render(){
     return(
-      <View>
-        <Text>Add a food that you are allergic/intolerant to:</Text>
-        <TextInput></TextInput>
-        <TouchableOpacity>Add</TouchableOpacity>
-        <FlatList></FlatList>
+      <View style={styles.container}>
+        <Text style={styles.text}>Add a food that you are allergic/intolerant to:</Text>
+        <TextInput style={styles.input} onChangeText={text => {let{textInputs} = this.state; textInputs[index] = text; this.setState({
+          textInputs,
+        });
+      }} calue={this.state.textInputs[index]}></TextInput>
+        <TouchableOpacity style={styles.button} onPress={() => {
+        let {textInputs} = this.state; textInputs[index] = ''; this.setState({
+          textInputs,
+        });
+      }}>Clear</TouchableOpacity>
+        <FlatList style={{flex: 1}} data={[1,2,3,4,5]} renderItem={({item, index}) => {
+          return (
+            <View style={{height: 100, backgroundColor: '#F0F0F0', width: 300, alignSelf: 'center', margin: 10,
+          }}></View>
+          )
+        }}></FlatList>
       </View>
     )
   }
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+
+  text: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'black',
+    fontSize: 20,
+    flexWrap: 'wrap',
+  },
+
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    width: 75,
+    backgroundColor: '#2c3539',
+  },
+
+  input: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 300,
+    flex: 1,
+  },
+});
