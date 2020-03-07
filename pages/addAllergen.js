334019fc4a8053bd876fc12b32281628db81224c
AoifeMcNeill <amcneill849@gmail.com>
2020-03-07 17:07:33 +0000

    let's try this tutorial

--- pages/addAllergen.js
+++ pages/addAllergen.js
@@ -1,75 +1,27 @@
 import React, {Component} from 'react';
+import {AppRegistry, View, Text, Button, TextInput} from 'react-native';
 
-import {
-  StyleSheet,
-  FlatList,
-  Text,
-  View,
-  TouchableOpacity,
-  TextInput,
-} from 'react-native';
-
-export default class addAllergen extends Component {
-  constructor() {
-    super();
-    state = {
-        textInputs: [],
+class addAllergen extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      textInput : []
     }
-  };
+  }
 
+  addTextInput = (key) => {
+    let textInput = this.state.textInput;
+    textInput.push(<TextInput key={key} />);
+    this.setState({textInput})
+  }
   render(){
     return(
-      <View style={styles.container}>
-        <Text style={styles.text}>Add a food that you are allergic/intolerant to:</Text>
-        <TextInput style={styles.input} onChangeText={text => {let{textInputs} = this.state; textInputs[index] = text; this.setState({
-          textInputs,
-        });
-      }} calue={this.state.textInputs[index]}></TextInput>
-        <TouchableOpacity style={styles.button} onPress={() => {
-        let {textInputs} = this.state; textInputs[index] = ''; this.setState({
-          textInputs,
-        });
-      }}>Clear</TouchableOpacity>
-        <FlatList style={{flex: 1}} data={[1,2,3,4,5]} renderItem={({item, index}) => {
-          return (
-            <View style={{height: 100, backgroundColor: '#F0F0F0', width: 300, alignSelf: 'center', margin: 10,
-          }}></View>
-          )
-        }}></FlatList>
+      <View>
+        <Button title='+' onPress={() => this.addTextInput(this.state.textInput.length)} />
+        {this.state.textInput.map((value, index) => {
+          return value
+        })}
       </View>
     )
   }
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 10,
-  },
-
-  text: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'black',
-    fontSize: 20,
-    flexWrap: 'wrap',
-  },
-
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 10,
-    width: 75,
-    backgroundColor: '#2c3539',
-  },
-
-  input: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 300,
-    flex: 1,
-  },
-});
+}
